9c3f649bcd4c394a9671afd5a63736f2ff580875
Lucas <lucas.porto@aisdigital.com.br>
2018-08-27 13:19:32 -0300

    Atualizações estrutura

---
+++ src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import createReactClass from 'create-react-class';
+import App_title from './Title';
+import React from 'react';
+
+export default createReactClass({
+    render: () => {
+        return (
+            <div>
+                <App_title name="Lucasc" />
+            </div>
+        )
+    }
+});
+
